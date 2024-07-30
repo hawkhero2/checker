3fc611f172edc5e7ff760fd4274049a639b0819b
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2024-07-30 23:31:23 +0300

    wip

--- checker.py
+++ checker.py
@@ -1,18 +1,22 @@
-import subprocess
 import argparse
+import pandas
+from datetime import datetime
 
 argum_parser = argparse.ArgumentParser(description='Test')
 
 argum_parser.add_argument('--source', type=str, help='Source file')
 argum_parser.add_argument('--destination', type=str, help='Destination file')
-argum_parser.add_argument('--help', type=str, help='Destination file')
 # http://pymotw.com/2/argparse/
 
 args = argum_parser.parse_args()
 
-source = args.source
-destination = args.destination
-help = args.help
+try:
+    source_df = pandas.read_csv(args.source)
+    destination_df = pandas.read_csv(args.destination)
 
-if help != None:
-    print(help)
+except Exception as e:
+    print("\nNo such file.\nCheck the path and try again.\nError Stack:\n")
+    print(e)
+
+mydate = datetime.strptime(str(source_df['date'].tail()),'%d/%m/%Y')
+print(mydate)
